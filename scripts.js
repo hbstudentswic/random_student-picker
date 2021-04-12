a85f6afa17fc3218c9f081ea212c35fde07507ad
Hunter Bossetto <hbstudentswic@gmail.com>
2021-04-12 12:05:22 -0500

    Add review comments

--- scripts.js
+++ scripts.js
@@ -9,10 +9,22 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is exclusive and the minimum is inclusive
 }
 
+/* "submit" is on the form NOT on a submit button; submit goes with a form */
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  /*
+      1. event.target is the form that got submitted
+      2. FormData uses values and name attributes from the form. INPUTS HAVE TO HAVE NAME ATTRIBUTES.
+      3. Create a new object from the entries in the form.
+      */
   const newStudent = Object.fromEntries(new FormData(event.target));
+  /*
+  LONGER VERSION OF LINE 19
+  const newStudent = {};
+  newStudent.fname = event.target.elements[0].value;
+  newStudent.lname = event.target.elements[0].value;
+  */
 
   console.log(newStudent);
 
